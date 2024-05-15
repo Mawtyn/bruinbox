92f6ed21eef40bf73ea16f094627179a521fccc8
Martin Lusinyan <lusinyanmartin3@gmail.com>
2024-05-15 02:38:20 -0700

    We back

---
+++ src/app/_components/Categories/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import classes from './index.module.scss'
+import Link from 'next/link'
+import { Category } from '../../../payload/payload-types'
+import CategoryCard from './CategoryCard'
+
+const Categories = ({ categories }: {categories: Category[] }) => {
+  return (
+    <section className={classes.container}>
+      <div className={classes.titleWrapper}>
+        <h2>Our Pricing</h2>
+        <Link href="/">Show All</Link>
+      </div>
+
+      <div className={classes.list}>
+        {categories.map((category) => (
+          <CategoryCard key={category.id}
+          category={category} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Categories
